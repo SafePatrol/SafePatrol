46e6ea0a6662899dcb9c0f1cb0054a6ab06182eb
Shin <kianms10310@naver.com>
2017-05-14 21:04:29 +0900

    Branch Test

--- app/src/main/java/com/hanict/safepatrol/MainActivity.java
+++ app/src/main/java/com/hanict/safepatrol/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // 수정 테스트
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
