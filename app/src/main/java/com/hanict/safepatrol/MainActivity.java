ce0e28eea19692d64ea2e33f59b280b8ce87b7af
Shin <kianms10310@naver.com>
2017-05-15 12:14:37 +0900

    메인 엑티비티 함수수정

--- app/src/main/java/com/hanict/safepatrol/MainActivity.java
+++ app/src/main/java/com/hanict/safepatrol/MainActivity.java
@@ -4,29 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-
     // 수정 테스트
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void onContentChanged() {
-        super.onContentChanged();
     }
 }
