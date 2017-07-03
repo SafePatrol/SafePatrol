b3b0d6c51dee0c5093c1d8fd3fb310e67fc9d5ab
HanSeonWoo <suh1580@naver.com>
2017-07-03 23:59:52 +0900

    Han Commit(2017-07-03, 사고 신고 엑티비티 추가)

---
+++ app/src/main/java/com/hanict/safepatrol/Report.java
@@ -0,0 +1,17 @@
+package com.hanict.safepatrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by suh15 on 2017-07-03.
+ */
+
+public class Report extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+
+}
