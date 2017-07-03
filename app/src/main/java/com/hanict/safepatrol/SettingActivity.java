a36c08ec6d020ce0f3f8859e8e2381fb5e64ae4c
HanSeonWoo <suh1580@naver.com>
2017-07-03 22:58:41 +0900

    Han Commit(2017-07-03)

--- app/src/main/java/com/hanict/safepatrol/SettingActivity.java
+++ app/src/main/java/com/hanict/safepatrol/SettingActivity.java
@@ -1,8 +1,17 @@
 package com.hanict.safepatrol;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Shin on 2017-06-23.
  */
 
-public class SettingActivity {
+public class SettingActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
 }
