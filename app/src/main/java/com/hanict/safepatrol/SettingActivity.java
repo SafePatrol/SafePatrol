39ecbecee9d6af4465e06e281471ff7053da372b
HanSeonWoo <suh1580@naver.com>
2017-07-10 22:00:40 +0900

    Han Commit(2017-07-10, 사고구역신고로 가는 버튼 이벤트 추가)

--- app/src/main/java/com/hanict/safepatrol/SettingActivity.java
+++ app/src/main/java/com/hanict/safepatrol/SettingActivity.java
@@ -1,7 +1,11 @@
 package com.hanict.safepatrol;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Shin on 2017-06-23.
@@ -12,6 +16,14 @@ public class SettingActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ImageButton Report = (ImageButton)findViewById(R.id.report_button);
+        Report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, Report.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
