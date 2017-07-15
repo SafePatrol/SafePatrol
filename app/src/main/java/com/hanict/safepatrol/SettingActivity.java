119655b8602e5828b74c7de09efe7ff0d4f09900
HanSeonWoo <suh1580@naver.com>
2017-07-15 18:33:34 +0900

    Han Commit(2017-07-15, 뒤로가기 이미지 버튼 추가 및 이벤트 추가)

--- app/src/main/java/com/hanict/safepatrol/SettingActivity.java
+++ app/src/main/java/com/hanict/safepatrol/SettingActivity.java
@@ -16,12 +16,21 @@ public class SettingActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        // 사고 다발 구역 신고 버튼 클릭시 해당 레이아웃으로 이동 코드
         ImageButton Report = (ImageButton)findViewById(R.id.report_button);
         Report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SettingActivity.this, Report.class);
                 startActivity(intent);
+            }
+        });
+        // 뒤로 가는 이미지 버튼 클릭시 해당 레이아웃을 종료
+        ImageButton ReverseButton = (ImageButton)findViewById(R.id.reverse_button);
+        ReverseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
