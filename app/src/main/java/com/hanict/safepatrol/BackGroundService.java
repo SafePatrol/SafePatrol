c4ba874ab4525873f4f0ac669b79da6ead4ec700
HanSeonWoo <suh1580@naver.com>
2017-07-27 13:03:15 +0900

    Han Commit(2017-07-15, Background Activity 주석추가)

--- app/src/main/java/com/hanict/safepatrol/BackGroundService.java
+++ app/src/main/java/com/hanict/safepatrol/BackGroundService.java
@@ -8,9 +8,13 @@ import android.support.annotation.Nullable;
 /**
  * Created by suh15 on 2017-07-18.
  * 백그라운드 관련 소스
+ * 추가할 항목
+ * 1. 사용자 위치와 사고다발구역 위치가 같으면 사용자가 설정한 사운드를 내도록 설정 (안드로이드 내부 DB사용)
+ * 2. 사용자의 설정한 시간 외에 사운드가 울리지 않도록 설정 (안드로이드 내부 DB사용)
  */
 
 public class BackGroundService extends Service{
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
