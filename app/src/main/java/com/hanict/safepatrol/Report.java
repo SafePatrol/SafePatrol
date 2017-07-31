91351426f9da8351351a32ca8a8e8edee1a4896d
HanSeonWoo <suh1580@naver.com>
2017-07-31 18:19:37 +0900

    Han Commit(2017-07-15, 사고 신고 기능 중 사진업로드 기능 추가)

--- app/src/main/java/com/hanict/safepatrol/Report.java
+++ app/src/main/java/com/hanict/safepatrol/Report.java
@@ -1,23 +1,105 @@
 package com.hanict.safepatrol;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by suh15 on 2017-07-03.
+ * 사진 업로드 관련 사이트 http://jeongchul.tistory.com/287
  */
 
 public class Report extends Activity {
+    private static final int PICK_FORM_CAM = 0;
+    private static final int PICK_FORM_ALB = 1;
+    private static final int CROP_FORM_IMA = 2;
+
+    private Uri mlmageCaptureUri;
+    private String absoultePath;
+
     private boolean SetDateButton = false;
     private boolean SetTimeButton = false;
     private String[] ReportList = {"차량과속", "신호위반", "보행자사고", "기타", "기타", "기타"};
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode != RESULT_OK)
+            return;
+        switch(requestCode) {
+            case PICK_FORM_ALB: {
+                mlmageCaptureUri = data.getData();
+                Log.d("smartWheel", mlmageCaptureUri.getPath().toString());
+            }
+            case PICK_FORM_CAM: {
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mlmageCaptureUri, "image/*");
+
+                //CROP할 이미지를 300 * 300으로 저장
+                intent.putExtra("outputX", 300);
+                intent.putExtra("outputY", 300);
+                intent.putExtra("aspectX", 1);
+                intent.putExtra("aspectY", 1);
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FORM_IMA);
+                break;
+            }
+            case CROP_FORM_IMA: {
+                if (resultCode != RESULT_OK) {
+                    return;
+                }
+                final Bundle extras = data.getExtras();
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/smartWheel" + System.currentTimeMillis() + ".jpg";
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    ImageView ReportImageview = (ImageView) findViewById(R.id.report_Imageview);
+                    ReportImageview.setImageBitmap(photo);
+                    String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/smartWheel";
+                    File directory_smartWheel = new File(dirPath);
+
+                    if (!directory_smartWheel.exists())
+                        directory_smartWheel.mkdir();
+                    File copyFile = new File(filePath);
+                    BufferedOutputStream out = null;
+                    try {
+                        copyFile.createNewFile();
+                        out = new BufferedOutputStream(new FileOutputStream(copyFile));
+                        photo.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(copyFile)));
+                        out.flush();
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    absoultePath = filePath;
+                    break;
+                }
+                File f = new File(mlmageCaptureUri.getPath());
+                if(f.exists()) {
+                    f.delete();
+                }
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,7 +171,27 @@ public class Report extends Activity {
         // 지도에서 찾기 버튼 이벤트(추가예정)
 
         // 갤러리에서 이미지 불러오기(추가예정)
-
-
+        Button ReportImageButton = (Button)findViewById(R.id.report_imagebutton);
+        ReportImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FORM_ALB);
+            }
+        });
+        // 카메라에서 이미지 불러오기
+        Button ReportCamButton = (Button)findViewById(R.id.report_cambutton);
+        ReportCamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // 임시로 경로를 생성
+                String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+                mlmageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+                intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mlmageCaptureUri);
+                startActivityForResult(intent, PICK_FORM_CAM);
+            }
+        });
     }
 }
